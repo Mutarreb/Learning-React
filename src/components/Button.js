bda8eaac70f49c08a322d60d0d6af7a84c4d2d7f
Ahmed Mutarreb <muttamutarreb@gmail.com>
2021-04-20 12:20:26 +1000

    First commit

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+export function Button (props) {
+    //store number of clicks 
+    const [click, setClick] = useState(0)
+    const update = () => {
+        setClick ( click + 1 )
+    }
+    return (
+        <button className = "button" onClick = {update}>
+            { "Clicked " + click + " times" }
+        </button>
+    )
+
+}
+export default Button 
+
---
+++ src/components/Text.js
@@ -0,0 +1,11 @@
+// Text component
+export function Text ( props ) {
+
+    return (
+        <p className = "text"> 
+        { props.name }
+        </p>
+    )
+}
+
+export default Text
