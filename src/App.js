50bd803c0ffd7c3d06373235dc99df63fb6fb4c5
Ahmed Mutarreb <muttamutarreb@gmail.com>
2021-04-27 12:34:51 +1000

    Logos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-import logo from './logo.svg';
+import Logo from './igloo-solid.png'
+import logo from './sorrento_by_the_sea_logo.jpg'
 import './App.css';
-import {Text} from './components/Text'
-import {Button} from './components/Button'
-import {List} from './components/List'
+import {Header} from './components/Header'
 
-const items = [
-  {"name" : "banana", "colour":" yellow"},
-  {"name" : "apple", "colour":" red"},
-  {"name" : "grapes", "colour":" green"},
+const NavItems = [
+  { "name": "Home", "link": " /home" },
+  { "name": "The Apartment", "link": "/apartment" },
+  { "name": "Pricing", "link": " /pricing"},
+  { "name": "Book", "link": " /book" },
+  { "name": "Local Area", "link": " /link"},
+  { "name": "Contact", "link": " /contact" },
 ]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <Text name = "barry"color ="yellow"/>
-        <Text name = "larryy" color ="red"/>
-        <Text name ="Ahmed" color =" gold" />
-        <Button start= {3}/>
-        <Button start = {100} />
-        <Button />
-        <List items_list = {items}/>
+    <div className=" website">
+      <Header logo = {Logo} /> 
+      <Header logo = {logo} />
+      <main className="content "></main>`
+      <footer className="fotter"> </footer>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+
   );
 }
 
