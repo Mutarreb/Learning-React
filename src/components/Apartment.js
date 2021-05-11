6c73d77ae26144764cfa7775a1b17e1b7b72bb04
Ahmed Mutarreb <muttamutarreb@gmail.com>
2021-05-11 12:44:17 +1000

    styling

--- src/components/Apartment.js
+++ src/components/Apartment.js
@@ -1,8 +1,62 @@
-export function Apartment (props) {
+import "../styles/Apartment.css"
+import {Banner} from './Banner'
+import BannerImage from '../banners/britt-gaiser-eNzBGf4fucc-unsplash.jpg'
+
+export function Apartment(props) {
     return (
-        <div className ="Apartment">
-            <h2>Apartment</h2>
+        <div className="Apartment">
+            <Banner image ={BannerImage} text ="Sorrento Apartment"/>
+            <h2>The Apartment</h2>
+            <div className="content">
+
+                <li> We offer a spacious living area overlooking a beautiful garden Modern and elegant bathroom </li>
+                <li>Queen sized bedroom suite </li>
+                <li>private entrance allows you to come and go as you please.</li>
+                 <li>Car parking is close by with room for a boat as well!</li>
+                 <li> Your luxury apartment consists of a spacious separate living area with leather furniture overlooking a private garden for you to enjoy.</li>
+                 <li>A separate Queen sized bedroom suite with quality linen also overlooks the garden. The modern, elegant bathroom has a luxurious bath that entices you to unwind and relax whilst overlooking a private courtyard fernery.</li>
+                <p> The kitchenette provides everything you will need for a relaxing breakfast. We include a coffee machine and a variety of coffee pods for your enjoyment. Two TVS will keep everyone happy along with a DVD player, stereo system and games galore.</p>
+                <p> There is an undercover area complete with BBQ and shady seating that offers plenty of space to stretch out and enjoy that cup of freshly brewed coffee or glass of wine.</p>
+                <p> Sit back and relax and take a breather. Read a book, go for a beach walk, have a sleep-in. It’s your private space to enjoy. </p>
+                <p>Amenities include top quality bedding, fluffy towels, bath robes, vanity packs, hair dryer, beach towels and chairs, a picnic basket, coffee machine and pods, a microwave/convection oven, a toaster, a kettle, a sandwich maker and BBQ tools.</p>
+                <p>We offer locally sourced provisions for a hearty breakfast together with home-made biscuits and chocolates. </p>
+
+
+            </div>
+            <div>
+
+            <article className="card">
+                <h4>Article one </h4>
+                <p>
+                    This is article one
+                    </p>
+            </article>
+
+            <article className="card">
+                <h4>Article two</h4>
+                <p>
+                    this is artcle two </p>
+
+            </article>
+
+            <article className="card">
+                <h4>Article three</h4>
+                <p>
+                    this is article three </p>
+            </article>
+
+            <article className="card">
+                <h4>Article four</h4>
+                <p>
+                    this video shows the apartment and the local area  </p>
+
+
+            </article>
+            </div>
+
+
         </div>
+
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,58 +9,15 @@ export function Home(props) {
 
             </div>
             <div className="content">
-                Our apartment is a lazy 10-minute walk from the relaxed township of Sorrento that offers restaurants, cafes, a chemist, a supermarket, a butcher and lots more. It is located in a quiet avenue, just 4 houses from the beach. It is in easy walking distance of the ocean beaches and surf.
+                <p>Our apartment is a lazy 10-minute walk from the relaxed township of Sorrento that offers restaurants, cafes, a chemist, a supermarket, a butcher and lots more.
+                It is located in a quiet avenue, just 4 houses from the beach. It is in easy walking distance of the ocean beaches and surf.</p>
 
-                Your private luxury apartment consists of a spacious living area with leather lounge suite overlooking a sun drenched, tree-filled private garden.
+                <p>Your private luxury apartment consists of a spacious living area with leather lounge suite overlooking a sun drenched, tree-filled private garden.</p>
 
-                A separate kitchenette has everything you need to make your stay relaxed and comfortable. A queen sized bedroom and an amazing bathroom complete with free standing black bath entices you to relax and unwind.
+                <p>A separate kitchenette has everything you need to make your stay relaxed and comfortable. A queen sized bedroom and an amazing bathroom complete with free standing black bath entices you to relax and unwind.</p>
 
-                Two TVs are sure to make everyone happy!  An undercover BBQ area with seating offers plenty of space to stretch out and enjoy that cup of freshly brewed coffee or a glass of wine.
-
-                This is the perfect place for a home away from home.
-</div>
-
-
-            {/* <article class="card">
-    <h4>About us </h4>
-    <p>
-    On the Mornington Peninsula (one hour from Melbourne) next door to Portsea.
-
-
-    </p>
-</article>
-
-<article class="card">
-    <h4>Article two</h4>
-    <p>
-        How to get here
-    </p>
-</article>
-
-<article class="card">
-    <h4>Article three</h4>
-    <p>
-        this is article three
-    </p>
-</article>
-
-<article class="card">
-    <h4>Article four</h4>
-    <p>
-        this video shows the apartment and the local area
-    </p>
-    <iframe className = "video" width="560" height="315" src="https://www.youtube.com/embed/KE0SbDBw-ok" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-</article>
-
-<article class="card">
-    <h4>Article five</h4>
-    <p>
-        this is article five
-    </p>
-    
-
-</article> */}
+                <p>Two TVs are sure to make everyone happy!  An undercover BBQ area with seating offers plenty of space to stretch out and enjoy that cup of freshly brewed coffee or a glass of wine.</p>
+                <p> This is the perfect place for a home away from home.</p></div>
 
         </div>
     )
