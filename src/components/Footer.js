e08fda798e71d276e951ac1c53b48dcc226f1657
Ahmed Mutarreb <muttamutarreb@gmail.com>
2021-05-25 11:43:10 +1000

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,24 @@
 import '../styles/Footer.css'
 export function Footer(props) {
   return (
-    <div className="Footer">
-      <footer class="main-footer">
-        
 
-        <nav class="social-nav">
-          <a href="https://facebook.com/sorrento">
-            <i class="fab fa-facebook-square"></i>
-          </a>
-          <a href="https://instagram.com/sorrento">
-            <i class="fab fa-instagram-square"></i>
-          </a>
-          <a href="https://twitter.com/sorrento">
-            <i class="fab fa-twitter-square"></i>
-          </a>
-          
-          <p class="copyright">&copy; Sorrento by the Sea</p>
-  
-        </nav>
-      </footer>
-    </div>
+    <footer class="main-footer">
+
+      
+      <nav class="social-nav">
+        <a href="https://facebook.com/sorrento">
+          <i class="fab fa-facebook-square"></i>
+        </a>
+        <a href="https://instagram.com/sorrento">
+          <i class="fab fa-instagram-square"></i>
+        </a>
+        <a href="https://twitter.com/sorrento">
+          <i class="fab fa-twitter-square"></i>
+        </a>
+      </nav>
+      <p class="copyright">&copy; Sorrento by the Sea</p>
+    </footer>
+
   )
 }
 
