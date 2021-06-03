9b87817e9ded96e87fe2e8a6640fed81bdbf7b2f
ahmed mutarreb <muttamutarreb@gmail.com>
2021-06-03 17:09:11 +1000

    home push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,33 @@ export function Footer(props) {
 
     <footer class="main-footer">
 
-      
+
       <nav class="social-nav">
-        <a href="https://facebook.com/sorrento">
-          <i class="fab fa-facebook-square"></i>
-        </a>
-        <a href="https://instagram.com/sorrento">
-          <i class="fab fa-instagram-square"></i>
-        </a>
-        <a href="https://twitter.com/sorrento">
-          <i class="fab fa-twitter-square"></i>
-        </a>
+        
+        <ul>
+        <h4 class ="section-name"> Social Media </h4>
+        <li><a href="https://facebook.com/sorrento">
+          <i class="fab fa-facebook-square"></i> Facebook
+        </a></li>
+        <li><a href="https://instagram.com/sorrento">
+          <i class="fab fa-instagram-square"></i> Instagram
+        </a></li>
+        <li> <a href="https://twitter.com/sorrento">
+          <i class="fab fa-twitter-square"></i> Twitter
+        </a> </li>
+        </ul>
       </nav>
-      <p class="copyright">&copy; Sorrento by the Sea</p>
+
+      
+
+      <div class="address"> 
+      <ul> <h4>Address</h4> 
+       <li>Sorrento by the Sea </li>
+      <li>42 Holyrood Avenue</li>
+      <li>Sorrento VIC 3943</li> 
+      <li>mobile: 0435 123 456</li></ul> </div>
+
+      <p class="copyright">&copy; 2021 Sorrento by the Sea</p>
     </footer>
 
   )
